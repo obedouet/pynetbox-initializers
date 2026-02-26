0735f6b1273cabda20dcd7bd34e688c898e807da
obedouet <obedouet@users.noreply.github.com>
2026-02-26 14:05:28 +0100

    fix: manage items which do not use 'name'

--- nb_init/initializers.py
+++ nb_init/initializers.py
@@ -7,7 +7,7 @@ from typing import Dict, Any, Optional
 import pynetbox
 
 from nb_init.api import NetboxAPI
-
+from nb_init.nb_naming import get_unique_name
 
 class NetboxInitializer:
     """Handles initialization of Netbox entities from YAML configuration files.
@@ -63,7 +63,7 @@ class NetboxInitializer:
         "contact_roles",
         "contacts"
         ]
-    
+
     def __init__(self, api: pynetbox.api, yaml_dir: str = "yaml"):
         """Initialize the Netbox initializer.
         
@@ -114,17 +114,20 @@ class NetboxInitializer:
                     item_data.update({'name':item_name})
                 self._create_item(entity_name, item_name, item_data)
         else:
+            id_field='name'
             for item_data in data:
                 if not 'name' in item_data:
-                    print(f"Warning missing name in {entity_name}")
-                    continue
-                self._create_item(entity_name, item_data['name'], item_data)
+                    id_field=get_unique_name(entity_name, item_data)
+                    if id_field is None:
+                        print(f"Warning missing name in {entity_name}")
+                        continue
+                self._create_item(entity_name, item_data[id_field], item_data)
                         
-    def _create_item(self, entity_name, item_name: str, item_data: Dict[str, Any]):
+    def _create_item(self, entity_name: str, item_name: str, item_data: Dict[str, Any]):
         """Create or get an item in Netbox.
 
         Args:
-            endpoint: pynetbox endpoint
+            entity_name: netbox model name
             item_name: Name of the item
             item_data: Item data
         """
@@ -145,3 +148,6 @@ class NetboxInitializer:
             print(f"Error processing {item_name}: {e}")
         except Exception as e:
             print(f"Unexpected error processing {item_name}: {e}")
+
+
+
